5ba5e5d558cfc27169df224af39787402e1eca3e
ghazalmdhmdh <113705021+ghazalmdhmdh@users.noreply.github.com>
2023-01-06 03:06:32 +0330

    Add files via upload

---
+++ 8.cs
@@ -0,0 +1,14 @@
+﻿using  System;
+namespace consoleApp4
+{
+    class boolean
+    {
+        static void Main(string[] args)
+        {
+            bool content=true;
+            bool nocontent=false;
+            Console.WriteLine("text is{0}",content);
+            Console.WriteLine("text is{0}",nocontent);
+        }
+    }
+}
