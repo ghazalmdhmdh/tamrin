f6f79ef43d5af0fca49826c480d7528cdc593fd5
ghazalmdhmdh <113705021+ghazalmdhmdh@users.noreply.github.com>
2023-01-06 03:03:40 +0330

    Add files via upload

---
+++ 6.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class program{
+    public static void Main(){
+        Console.WriteLine("please enter your number:");
+        string num=Console.ReadLine();
+        double number=Convert.ToDouble(num);
+        double res=calc(number);
+         Console.WriteLine("pow2:");
+        Console.WriteLine(res);
+         double res_2=calc2(number);
+           Console.WriteLine("pow3:");
+        Console.WriteLine(res_2);
+    }
+    private static double calc(double x){
+        double a=Math.Pow(x,2);
+        return a;
+        double b=Math.Pow(x,3);
+        return b;
+    }
+    
+}
